85062dfeebe7409a8b664952c4a852927a215408
Caitlin Chan <cdechan@ucdavis.edu>
2025-03-26 00:00:05 -0700

    add tailwind config

---
+++ tailwind.config.mjs
@@ -0,0 +1,17 @@
+import defaultTheme from "tailwindcss/defaultTheme";
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/app/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["var(--font-inter)", ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  plugins: [],
+};
