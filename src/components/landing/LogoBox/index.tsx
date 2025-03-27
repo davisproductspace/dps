355a7f80d93d949a0d55b85c2e313e24efd20f4b
Caitlin Chan <cdechan@ucdavis.edu>
2025-03-27 16:23:29 -0700

    auto scale logos into boxes for fellowship

--- src/components/landing/LogoBox/index.tsx
+++ src/components/landing/LogoBox/index.tsx
@@ -7,8 +7,14 @@ interface LogoBoxProps {
 export default function LogoBox({ children }: LogoBoxProps) {
   return (
     <div className="w-[70px] h-[70px] flex items-center justify-center rounded-2xl bg-black shadow-[0_1px_2px_rgba(255,255,255,0.1)]">
-      <div className="w-6 h-6">
-        {children}
+      {/* auto-scaling container */}
+      <div className="flex items-center justify-center w-10 h-10">
+        <div className="w-full h-full flex items-center justify-center">
+          {/* ensures svg shrinks if needed */}
+          <div className="max-w-full max-h-full scale-100">
+            {children}
+          </div>
+        </div>
       </div>
     </div>
   );
